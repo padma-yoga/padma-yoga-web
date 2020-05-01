e1a4813aa095d057ceb0452ae95b9e7694468b99
Rodrigo Avellar <roavellarm@gmail.com>
2020-05-01 15:07:05 -0300

    Add theme

---
+++ src/theme/index.js
@@ -0,0 +1,37 @@
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import colors from './colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: colors.black,
+    },
+    secondary: {
+      main: colors.black,
+    },
+  },
+  typography: {
+    useNextVariants: true,
+    fontFamily: 'Roboto',
+    h1: {
+      fontSize: 64,
+      color: colors.black,
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+      lineHeight: '150%',
+      textAlign: 'center',
+      letterSpacing: '0.5px',
+    },
+    h2: {
+      fontSize: 24,
+      color: colors.black,
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+      lineHeight: 'normal',
+      textAlign: 'center',
+      letterSpacing: '0.5px',
+    },
+  },
+})
+
+export default theme
