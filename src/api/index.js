532a06cd563e3bf7c05dfa15a74329150907b1c2
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-01 12:28:06 -0300

    Add and Config Axios

---
+++ src/api/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const isLocalhost = window.location.origin.includes('localhost')
+
+axios.defaults.baseURL = isLocalhost
+  ? 'http://localhost:3001'
+  : 'https://staging-padma-yoga-api.herokuapp.com'
+
+axios.interceptors.request.use(
+  (config) => {
+    return { ...config, header: { 'Content-Type': 'application/json' } }
+  },
+  (error) => Promise.reject(error)
+)
+
+axios.interceptors.response.use(
+  (config) => config,
+
+  (error) => {
+    if (error.response === undefined) {
+      const networkError = JSON.parse(JSON.stringify(error))
+      return Promise.reject(networkError.message)
+    }
+
+    const { status, data } = error.response
+    if (status === 400) return Promise.reject(data)
+
+    if (status === 401) {
+      localStorage.clear()
+      window.location = '/'
+      return window.location.reload()
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default axios
