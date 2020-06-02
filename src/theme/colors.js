f0222c3d48479f4a220b997be16a63466e031759
Rodrigo Avellar <roavellarm@gmail.com>
2020-06-01 21:57:47 -0300

    Update colors theme

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,4 +1,14 @@
 export default {
+  primary: '#B71C1C',
+  secondary: '#F36868',
+  textPrimary: '#322f2f',
+  textSecondary: '#50565E',
+  silver: '#ECEFF1',
   black: '#444444',
   white: '#fafafa',
+  success: '#5ACC5A',
+  warning: '#FFC247',
+  danger: '#CC001B',
+  error: '#F20000',
+  background: 'red',
 }
