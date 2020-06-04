2fcf24148506425608a2d044175d2e4a8a527187
Gustavo <gustavosohne38@gmail.com>
2020-06-04 11:15:12 -0300

    table-menu

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react'
 import { getUsersAction } from 'actions/users'
 import Header from './Header'
 
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+
 function Users() {
   const [users, setUsers] = useState([])
 
@@ -16,25 +25,42 @@ function Users() {
     onGetUsers()
   }, [])
 
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  })
+
+  const classes = useStyles()
+
   return (
     <>
       <Header />
-      <h1>Lista de Usuários</h1>
-      {users.map((user) => {
-        return (
-          <div key={user.id}>
-            <div>
-              <strong>Email:</strong>
-              {` ${user.email}`}
-            </div>
-            <div>
-              <strong>Função:</strong>
-              {`${user.roles.map((role) => `${role} \n`)}`}
-            </div>
-            <br />
-          </div>
-        )
-      })}
+      <div align="center">
+        {' '}
+        <h1>Lista de Usuários</h1>
+      </div>
+
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Email</TableCell>
+              <TableCell align="left">Função</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell align="left">{user.email}</TableCell>
+                <TableCell align="left" component="th" scope="row">
+                  {user.roles}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
